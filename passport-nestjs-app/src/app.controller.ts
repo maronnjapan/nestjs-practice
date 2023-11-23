170291546bc146163d23183a7cc3fcecbbaa5419
maronnjapan <tihoutaikai20@gmail.com>
2023-11-23 07:36:45 +0000

    JwtAuthGuardのインポートし直し。

--- passport-nestjs-app/src/app.controller.ts
+++ passport-nestjs-app/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
