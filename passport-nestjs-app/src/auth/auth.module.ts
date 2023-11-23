3aa58e3da89c61be9beed0df98920b1e0c205923
maronnjapan <tihoutaikai20@gmail.com>
2023-11-23 04:28:09 +0000

    JWT発行処理実装

--- passport-nestjs-app/src/auth/auth.module.ts
+++ passport-nestjs-app/src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule, JwtModule.register({
+    global: true,
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+  }),],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule { }
